f13836ab4cd6224fc99c3c8d9700bd88a423ea88
Mark Oberemk <moberemk@gmail.com>
2015-07-04 13:38:24 -0400

    Adding abstract definition for Set type

---
+++ src/Set/Set.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace moberemk\LazyCollection\Set;
+
+/**
+ * A set is a collection of elements which are both ordered and unique;
+ * database resources should extend from here
+ */
+abstract class Set extends LazyCollection {}
